e8f963d4e8635559e911dd5e32c5e9a1a7abff6b
Artem Boichuk <82770812+Borfak@users.noreply.github.com>
2024-04-14 00:42:26 +0300

    status bar added

--- frontend/src/components/StatusBar.jsx
+++ frontend/src/components/StatusBar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-const StatusBar = ({ name, color, percentage }) => {
+const StatusBar = ({ name, percentage }) => {
   let colorClass;
   if (percentage <= 30) {
     colorClass = 'bg-red-500'; 
-  } else if (percentage <= 75) {
+  } else if (percentage <= 65) {
     colorClass = 'bg-yellow-500'; 
   } else {
     colorClass = 'bg-green-500'; 
   }
 
+  const progressWidth = `${100 - percentage}%`;
+
   return (
     <div className="flex justify-center space-x-4">
       <div className="text-lg">{name}</div>
-      <div className={`w-20 h-6 ${colorClass}`}></div>
-      <div className="text-lg">{percentage}%</div>
+      <div className={`border border-gray-400 w-24 h-8 relative overflow-hidden ${colorClass}`}>
+        <div style={{ width: progressWidth }} className="h-full bg-white"></div>
+      </div>
+      <div className={`text-lg ${colorClass}`}></div>
     </div>
   );
 };
