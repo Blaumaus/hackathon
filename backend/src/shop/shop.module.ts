3d656e34b89653c96a1b5c1602337b11ed297bc4
Blue Mouse <durexp@protonmail.ch>
2024-04-13 21:19:20 +0100

    fixed app start

--- backend/src/shop/shop.module.ts
+++ backend/src/shop/shop.module.ts
@@ -4,15 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShopService } from './shop.service';
 import { AquariumBuffs } from './entities/aquariumBuffs.entity';
 import { FisheriesEntity } from './entities/fisheries.entity';
-import { UserService } from 'src/user/user.service';
-import { AquariumService } from 'src/aquarium/aquarium.service';
 import { ShopController } from './shop.controller';
+import { UserModule } from 'src/user/user.module';
+import { AquariumModule } from 'src/aquarium/aquarium.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AquariumBuffs, FisheriesEntity]),
-    UserService,
-    AquariumService,
+    UserModule,
+    AquariumModule,
   ],
   providers: [ShopService],
   exports: [ShopService],
