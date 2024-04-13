eb7c856a9a27df12cac536697682b71da23fea3c
Blue Mouse <durexp@protonmail.ch>
2024-04-13 18:49:42 +0100

    fisheries shop entity

--- backend/src/shop/shop.service.ts
+++ backend/src/shop/shop.service.ts
@@ -2,13 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AquariumBuffs } from './entities/aquariumBuffs.entity';
+import { FisheriesEntity } from './entities/fisheries.entity';
 
 @Injectable()
 export class ShopService {
   constructor(
     @InjectRepository(AquariumBuffs)
     private aquariumBuffsRepository: Repository<AquariumBuffs>,
+    private fisheriesRepository: Repository<FisheriesEntity>,
   ) {}
+
+  async createFishery(entity: FisheriesEntity): Promise<FisheriesEntity> {
+    return this.fisheriesRepository.save(entity);
+  }
+
+  async updateFishery(
+    criteria: any,
+    update: Record<string, unknown>,
+  ): Promise<any> {
+    return this.fisheriesRepository.update(criteria, update);
+  }
+
+  async deleteFishery(id: string): Promise<any> {
+    return this.fisheriesRepository.delete(id);
+  }
+
+  async findFishery(params: any): Promise<FisheriesEntity[]> {
+    return this.fisheriesRepository.find(params);
+  }
+
+  async findOneFishery(where: any, params: any = {}): Promise<FisheriesEntity> {
+    return this.fisheriesRepository.findOne({
+      where,
+      ...params,
+    });
+  }
 
   async createBuff(entity: AquariumBuffs): Promise<AquariumBuffs> {
     return this.aquariumBuffsRepository.save(entity);
