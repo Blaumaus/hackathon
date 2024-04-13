fd1a1ced5a093320c06a70ae679db601f9863a27
Blue Mouse <durexp@protonmail.ch>
2024-04-13 19:12:06 +0100

    GET /consumables

--- backend/src/shop/shop.controller.ts
+++ backend/src/shop/shop.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAccessTokenGuard } from 'src/auth/guards/jwt-access-token.guard';
+import { CurrentUserId } from 'src/auth/decorators';
 import { ShopService } from './shop.service';
 
 @ApiTags('Shop')
 @Controller('v1/shop')
 export class ShopController {
   constructor(private readonly shopService: ShopService) {}
+
+  @Get('/consumables')
+  @UseGuards(JwtAccessTokenGuard)
+  async update(@CurrentUserId() uid: string): Promise<any[]> {}
 }
