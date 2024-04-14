d416c8f2feb3bf333a0cfce3e2eb574b5fdfafad
Blue Mouse <durexp@protonmail.ch>
2024-04-14 02:36:44 +0100

    config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        aquarium: 'aquarium 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        aquarium: {
+          '0%, 100%': { opacity: 0.9 },
+          '50%': { opacity: 0.75 },
+        }
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
